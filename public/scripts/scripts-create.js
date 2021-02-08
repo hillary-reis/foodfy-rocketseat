2ce79f64eb5b25d5e8949f631046806d31f683f8
Hillary Reis <hillary.reis2002@gmail.com>
2021-02-08 14:14:48 -0300

    confirmação de exclusão da receita

--- public/scripts/scripts-create.js
+++ public/scripts/scripts-create.js
@@ -32,3 +32,16 @@ function addPreparation () {
 };
 
 buttonPreparation.addEventListener("click", addPreparation);
+
+//================== confirmação no EDIT.NJK ==================
+
+const formDelete = document.querySelector('#form-delete');
+
+formDelete.addEventListener('submit', function (event) {
+  const confirmation = confirm ('Deseja realmente Deletar?');
+
+  if (!confirmation) {
+    event.preventDefault();
+  };
+
+});
